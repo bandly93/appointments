f92e5d7dbff6059fbe15e65fa9bd00a068401157
Band Ly <bandly93@gmail.com>
2026-07-17 23:37:59 -0700

    set up cors to handle front end api calls

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,11 @@
 import express, { type Express, type Request, type Response } from 'express';
+import cors from 'cors'
 import authRouter from './routes/authRoutes.js'
 
 const app: Express = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
